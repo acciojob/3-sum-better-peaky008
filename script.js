00a22d5bb99bebc5a6e177b065863b85b46c2b66
peaky008 <131166347+peaky008@users.noreply.github.com>
2023-06-13 16:01:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let maxi=0;
+	for(let i=0;i<arr.length;i++){
+		let sum=0;
+		for(let j=i;j<arr.length;j++){
+			sum+=arr[j];
+		}
+		maxi=Math.max(sum,maxi);
+	}
+  return maxi;
 }
 
 module.exports = threeSum;
